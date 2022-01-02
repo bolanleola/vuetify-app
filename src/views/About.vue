67bb43976e04dc5227951c546eebf44109798b84
Oladipo Omobolanle <41433596+bolanleola@users.noreply.github.com>
2022-01-02 20:37:20 +0100

    First Commit

---
+++ src/views/About.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="about pa-6">
+    <h1>About Vuetify Todo</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perferendis saepe vitae distinctio neque optio delectus tempora quia libero quo quasi rerum corrupti quidem consequatur omnis explicabo reprehenderit, aspernatur, numquam ab.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At quae, voluptatibus consequatur assumenda porro impedit vero laudantium iure tempora ut incidunt delectus ab aperiam quam, est exercitationem eum aspernatur provident.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium quo ex dolore? Quia cupiditate, sequi repellendus aperiam at vero debitis, vel eum inventore, vitae consequuntur quae magnam maiores deleniti pariatur.</p>
+  </div>
+</template>
